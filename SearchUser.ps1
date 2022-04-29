c5ad9114bf62e88ee00f12290887ff9dea7c8dfd
Psyzmic <82079347+Psyzmic@users.noreply.github.com>
2022-04-29 09:31:03 -0600

    Add files via upload

---
+++ SearchUser.ps1
@@ -0,0 +1,10 @@
+﻿#this script will search all specified domains to find the name of the user specified. You can use first and/or last name.
+
+$name = Read-Host "What is the user's name?" 
+
+    $ErrorActionPreference = ‘SilentlyContinue’
+
+        Get-ADUser -server <domain 1> -Filter "name -like '*$name*'" | select Name, sAMAccountName, DistinguishedName
+        Get-ADUser -server <domain 2> -Filter "name -like '*$name*'" | select Name, sAMAccountName, DistinguishedName
+        Get-ADUser -server <domain 3> -Filter "name -like '*$name*'" | select Name, sAMAccountName, DistinguishedName
+        Get-ADUser -server <domain 4> -Filter "name -like '*$name*'" | select Name, sAMAccountName, DistinguishedName
