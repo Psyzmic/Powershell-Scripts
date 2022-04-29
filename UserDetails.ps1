7f0937b0e9702d9a832cc59c8a0a0e2a91f191d5
Psyzmic <82079347+Psyzmic@users.noreply.github.com>
2022-04-29 08:55:29 -0600

    Add files via upload

---
+++ UserDetails.ps1
@@ -0,0 +1,17 @@
+﻿#This script searches all specified domains for the specified username and returns useful user properties.
+
+
+$username = Read-Host "What is the Username?" 
+
+     $ErrorActionPreference = ‘SilentlyContinue’
+
+        Get-ADUser -server <domain 1> -identity $username -properties * | select Name, DisplayName, title, DistinguishedName, Department, Company, l, Manager, telephoneNumber, ipPhone, mobilePhone, b, EmailAddress, PrimaryGroup, Enabled, AccountExpirationDate, PasswordLastSet, PasswordExpired -erroraction silentlycontinue
+
+        Get-ADUser -server <domain 2> -identity $username -properties * | select Name, DisplayName, title, DistinguishedName, Department, Company, l, Manager, telephoneNumber, ipPhone, mobilePhone, b, EmailAddress, PrimaryGroup, Enabled, AccountExpirationDate, PasswordLastSet, PasswordExpired Err -erroraction silentlycontinue
+    
+        Get-ADUser -server <domain 3> -identity $username -properties * | select Name, DisplayName, title, DistinguishedName, Department, Company, l, Manager, telephoneNumber, ipPhone, mobilePhone, b, EmailAddress, PrimaryGroup , Enabled, AccountExpirationDate, PasswordLastSet, PasswordExpired -erroraction silentlycontinue
+ 
+        Get-ADUser -server <domain 4> -identity $username -properties * | select Name, DisplayName, title, DistinguishedName, Department, Company, l, Manager, telephoneNumber, ipPhone, mobilePhone, b, EmailAddress, PrimaryGroup, Enabled, AccountExpirationDate, PasswordLastSet, PasswordExpired -erroraction silentlycontinue
+        
+        
+
