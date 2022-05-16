6be3afa59450a0f9f5c633c480d197b4a672afc3
Psyzmic <82079347+Psyzmic@users.noreply.github.com>
2022-05-16 09:58:10 -0600

    Add files via upload

---
+++ BulkSetExpiration.ps1
@@ -0,0 +1,12 @@
+﻿#bulk process account expiration requests from a csv file
+
+#define usercsv folder path before proceeding
+
+$userscsv = @('C:\users\ccatucci\users.csv') 
+$users = Import-Csv -Path "$userscsv" -Encoding UTF8
+$date = Read-Host "What is the expiration date? (Day/Month/Year 00:00:00)"
+$domain = Read-Host "What is the domain?"
+
+foreach ($user in $users) {
+    Set-AdUser -server $domain -Identity $user -Accountexpirationdate $date
+}
