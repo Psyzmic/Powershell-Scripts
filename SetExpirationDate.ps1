45ee6f3cf5e60587f96d4a29ce7e003b27f478ce
Psyzmic <82079347+Psyzmic@users.noreply.github.com>
2022-05-16 10:02:14 -0600

    Add files via upload

---
+++ SetExpirationDate.ps1
@@ -0,0 +1,9 @@
+﻿#process account expiration requests for a defined user
+
+$user = Read-Host "What is the username?"
+$date = Read-Host "What is the expiration date? (Day/Month/Year 00:00:00)"
+$domain = Read-Host "What is the domain?"
+
+
+Set-AdUser -server $domain -Identity $user -Accountexpirationdate $date
+Get-ADUser -identity $user -server $domain -properties * |select AccountExpirationDate
